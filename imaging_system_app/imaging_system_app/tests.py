e102b47e9dde6dcdbff7bc2bed1664391739aa66
Forbes Lai <2445570l@student.gla.ac.uk>
2021-11-23 02:42:52 +0000

    added unit test file

---
+++ imaging_system_app/imaging_system_app/tests.py
@@ -0,0 +1,6 @@
+import os
+import re
+
+# This is the unit test template
+FAILURE_HEADER = f"{os.linesep}{os.linesep}{os.linesep}================{os.linesep}TwD TEST FAILURE =({os.linesep}================{os.linesep}"
+FAILURE_FOOTER = f"{os.linesep}"
