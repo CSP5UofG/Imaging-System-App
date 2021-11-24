3530267ffffd5636775a6b1ca90ef2ea55e3ed6c
zsolttakacs2000 <zsolt.takacs2000@gmail.com>
2021-11-24 16:58:37 +0000

    fix - str function
    fixed str functions in Customer and Worker objects in models.py

--- imaging_system_app_project/imaging_system_app/models.py
+++ imaging_system_app_project/imaging_system_app/models.py
@@ -11,7 +11,7 @@ class Customer(models.Model):
     #service_id = models.ForeignKey(Services, on_delete=models.CASCADE)
     
     def __str__(self):
-        return self.cust_id + " " + self.cust_name
+        return str(self.cust_id) + " " + self.cust_name
 
 
 class Worker(models.Model):
@@ -22,6 +22,6 @@ class Worker(models.Model):
     cust_id = models.ForeignKey(Customer, on_delete = models.CASCADE)
     
     def __str__(self):
-        return self.worker_id + " " + self.worker_name + "works for: " + self.cust_id
+        return str(self.worker_id) + " " + self.worker_name + "works for: " + str(self.cust_id)
     
     
