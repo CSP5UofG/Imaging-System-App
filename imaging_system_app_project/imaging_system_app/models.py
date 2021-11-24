2145f7f81b6bb17033c1370a8bec42f5bb4a4147
zsolttakacs2000 <zsolt.takacs2000@gmail.com>
2021-11-24 16:39:19 +0000

    feat - Updated models
    Created Customer and Worker objects in models

--- imaging_system_app_project/imaging_system_app/models.py
+++ imaging_system_app_project/imaging_system_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    cust_id = models.AutoField(primary_key = True)
+    cust_name = models.CharField(max_length = 100)
+    cust_tel_no = models.CharField(max_length = 11) #specify if numbers can be international
+    cust_email = models.CharField(max_length = 100)
+    cust_budget_code = models.IntegerField()
+    #service_id = models.ForeignKey(Services, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.cust_id + " " + self.cust_name
+
+
+class Worker(models.Model):
+    worker_id = models.AutoField(primary_key = True)
+    worker_name = models.CharField(max_length = 100)
+    worker_tel_no = models.CharField(max_length = 11) #specify if numbers can be international
+    worker_email = models.CharField(max_length = 100)
+    cust_id = models.ForeignKey(Customer, on_delete = models.CASCADE)
+    
+    def __str__(self):
+        return self.worker_id + " " + self.worker_name + "works for: " + self.cust_id
+    
+    
