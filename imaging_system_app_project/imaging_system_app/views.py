db336deb6de49928b024401cf1bbbc2fda2af773
Forbes Lai <2445570l@student.gla.ac.uk>
2021-11-25 03:22:22 +0000

    feat - base view, templates and static
    created a basic view and template for home and services page, with functionality for static added

--- imaging_system_app_project/imaging_system_app/views.py
+++ imaging_system_app_project/imaging_system_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    context_dict = {}
+    return render(request, 'imaging_system_app/index.html', context=context_dict)
+
+def services(request):
+    context_dict = {}
+    return render(request, 'imaging_system_app/services.html', context=context_dict)
