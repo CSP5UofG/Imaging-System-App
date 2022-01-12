67e5f0a5f21d6459bfcb3af56fdd398f31dbf564
Forbes Lai <2445570l@student.gla.ac.uk>
2022-01-12 10:19:41 +0000

    fix: population script issues

--- imaging_system_app_project/imaging_system_app_project/settings.py
+++ imaging_system_app_project/imaging_system_app_project/settings.py
@@ -81,9 +81,10 @@ WSGI_APPLICATION = 'imaging_system_app_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR,"db.sqlite3")),
     }
 }
+
 
 
 # Password validation
