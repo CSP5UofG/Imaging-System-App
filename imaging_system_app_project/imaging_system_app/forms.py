6e67c344642ba83c59fe4bc2db6ff1e4ed728fe3
zsolttakacs2000 <zsolt.takacs2000@gmail.com>
2022-01-13 19:33:44 +0000

    fix: date input widget and minimum values set

--- imaging_system_app_project/imaging_system_app/forms.py
+++ imaging_system_app_project/imaging_system_app/forms.py
@@ -7,19 +7,32 @@ class ServicesForm(forms.ModelForm):
     cust_type = forms.CharField(max_length = 100,
                                 help_text = "Customer Type")
     discount = forms.FloatField(help_text = "Discount")
-    jeol1200tem_cost = forms.FloatField(help_text = "jeol1200tem cost")
-    jeol100sem_cost = forms.FloatField(help_text = "jeol100sem cost")
-    tem_processing_cost = forms.FloatField(help_text = "tem processing cost")
-    sectioning_stained_cost = forms.FloatField(help_text = "sectioning stained cost")
-    sectioning_contrast_stained_cost = forms.FloatField(help_text = "sectioning contrast stained cost")
-    negative_staining_cost = forms.FloatField(help_text = "negative staining cost")
-    sem_processing_mounting_cost = forms.FloatField(help_text = "sem processing mounting cost")
-    sem_processing_fd_cost = forms.FloatField(help_text = "sem processing fd cost")
-    sem_cost = forms.FloatField(help_text = "sem cost")
-    immunolabelling_cost = forms.FloatField(help_text = "immunolabelling cost")
-    cryosectioning_cost = forms.FloatField(help_text = "cryosectioning cost")
-    freeze_fracture_cost = forms.FloatField(help_text = "freeze fracture cost")
-    ir_white_cost = forms.FloatField(help_text = "ir white cost")
+    jeol1200tem_cost = forms.FloatField(help_text = "jeol1200tem cost",
+                                        min_value = 0)
+    jeol100sem_cost = forms.FloatField(help_text = "jeol100sem cost",
+                                       min_value = 0)
+    tem_processing_cost = forms.FloatField(help_text = "tem processing cost",
+                                           min_value = 0)
+    sectioning_stained_cost = forms.FloatField(help_text = "sectioning stained cost",
+                                               min_value = 0)
+    sectioning_contrast_stained_cost = forms.FloatField(help_text = "sectioning contrast stained cost",
+                                                        min_value = 0)
+    negative_staining_cost = forms.FloatField(help_text = "negative staining cost",
+                                              min_value = 0)
+    sem_processing_mounting_cost = forms.FloatField(help_text = "sem processing mounting cost",
+                                                    min_value = 0)
+    sem_processing_fd_cost = forms.FloatField(help_text = "sem processing fd cost",
+                                              min_value = 0)
+    sem_cost = forms.FloatField(help_text = "sem cost",
+                                min_value = 0)
+    immunolabelling_cost = forms.FloatField(help_text = "immunolabelling cost",
+                                            min_value = 0)
+    cryosectioning_cost = forms.FloatField(help_text = "cryosectioning cost",
+                                           min_value = 0)
+    freeze_fracture_cost = forms.FloatField(help_text = "freeze fracture cost",
+                                            min_value = 0)
+    ir_white_cost = forms.FloatField(help_text = "ir white cost",
+                                     min_value = 0)
     
     class Meta:
         model = Services
@@ -38,7 +51,8 @@ class CustomerForm(forms.ModelForm):
                                   help_text = "Customer Tel. no")
     cust_email = forms.CharField(max_length = 100,
                                  help_text = "Customer email address")
-    cust_budget_code = forms.IntegerField(help_text = "Budget code")
+    cust_budget_code = forms.IntegerField(help_text = "Budget code",
+                                          min_value = 0)
     service_id = forms.ModelChoiceField(queryset = Services.objects.all(),
                                         help_text = "Customer Type")
     
@@ -66,8 +80,10 @@ class WorkerForm(forms.ModelForm):
 class ProjectForm(forms.ModelForm):
     cust_id = forms.ModelChoiceField(queryset = Customer.objects.all(),
                                      help_text = "Customer Company")
-    project_date = forms.DateField(help_text = "Date")
-    num_samples = forms.IntegerField(help_text = "Number of Samples")
+    project_date = forms.DateField(help_text = "Date",
+                                   widget = forms.SelectDateWidget)
+    num_samples = forms.IntegerField(help_text = "Number of Samples",
+                                     min_value = 0)
     specimen_procedure = forms.CharField(max_length = 500,
                                          help_text = "Specimen procedure")
     chemical_fixation = forms.CharField(max_length = 100,
@@ -126,10 +142,12 @@ class WorkerProjectBridgeForm(forms.ModelForm):
 
 
 class BillForm(forms.ModelForm):
-    billing_date = forms.DateField(help_text = "Date")
+    billing_date = forms.DateField(widget = forms.SelectDateWidget,
+                                   help_text = "Date")
     billing_address = forms.CharField(max_length = 100,
                                       help_text = "Billing Address")
-    total_cost = forms.FloatField(help_text = "Total Cost")
+    total_cost = forms.FloatField(help_text = "Total Cost",
+                                  min_value = 0)
     cust_id = forms.ModelChoiceField(queryset = Customer.objects.all(),
                                      help_text = "Customer Company")
     
@@ -141,31 +159,48 @@ class BillForm(forms.ModelForm):
 class ProjectBillDetailsForm(forms.ModelForm):
     project_id = forms.ModelChoiceField(queryset = Project.objects.all(),
                                      help_text = "Choose Project")
-    jeol1200tem_unit = forms.FloatField(help_text = "jeol1200tem unit")
-    jeol100sem_unit = forms.FloatField(help_text = "jeol100sem unit")
-    tem_processing_unit = forms.IntegerField(help_text = "TEM processing unit")
-    sectioning_stained_unit = forms.IntegerField(help_text = "Sectioning stained unit")
-    sectioning_contrast_stained_unit = forms.IntegerField(help_text = "Sectioning contrast stained unit")
-    negative_staining_unit = forms.IntegerField(help_text = "Negative staining unit")
-    sem_processing_mounting_unit = forms.IntegerField(help_text = "SEM processing mounting unit")
-    sem_processing_fd_unit = forms.IntegerField(help_text = "SEM processing fd unit")
-    sem_unit = forms.IntegerField(help_text = "SEM unit")
-    immunolabelling_unit = forms.IntegerField(help_text = "Immunolabelling unit")
-    cryosectioning_unit = forms.IntegerField(help_text = "Cryosectioning unit")
-    freeze_fracture_unit = forms.IntegerField(help_text = "Freeze fracture unit")
-    ir_white_unit = forms.IntegerField(help_text = "ir white unit")
+    jeol1200tem_unit = forms.FloatField(help_text = "jeol1200tem unit",
+                                        min_value = 0)
+    jeol100sem_unit = forms.FloatField(help_text = "jeol100sem unit",
+                                       min_value = 0)
+    tem_processing_unit = forms.IntegerField(help_text = "TEM processing unit",
+                                             min_value = 0)
+    sectioning_stained_unit = forms.IntegerField(help_text = "Sectioning stained unit",
+                                                 min_value = 0)
+    sectioning_contrast_stained_unit = forms.IntegerField(help_text = "Sectioning contrast stained unit",
+                                                          min_value = 0)
+    negative_staining_unit = forms.IntegerField(help_text = "Negative staining unit",
+                                                min_value = 0)
+    sem_processing_mounting_unit = forms.IntegerField(help_text = "SEM processing mounting unit",
+                                                      min_value = 0)
+    sem_processing_fd_unit = forms.IntegerField(help_text = "SEM processing fd unit",
+                                                min_value = 0)
+    sem_unit = forms.IntegerField(help_text = "SEM unit",
+                                  min_value = 0)
+    immunolabelling_unit = forms.IntegerField(help_text = "Immunolabelling unit",
+                                              min_value = 0)
+    cryosectioning_unit = forms.IntegerField(help_text = "Cryosectioning unit",
+                                             min_value = 0)
+    freeze_fracture_unit = forms.IntegerField(help_text = "Freeze fracture unit",
+                                              min_value = 0)
+    ir_white_unit = forms.IntegerField(help_text = "ir white unit",
+                                       min_value = 0)
     
     extra1_name = forms.CharField(max_length = 100,
                                   help_text = "First extra service name")
-    extra1_cost = forms.FloatField(help_text = "First extra service cost")
+    extra1_cost = forms.FloatField(min_value = 0,
+                                   help_text = "First extra service cost")
     extra2_name = forms.CharField(max_length = 100,
                                   help_text = "Second extra service name")
-    extra2_cost = forms.FloatField(help_text = "Second extra service cost")
+    extra2_cost = forms.FloatField(min_value = 0,
+                                   help_text = "Second extra service cost")
     extra3_name = forms.CharField(max_length = 100,
                                   help_text = "Third extra service name")
-    extra3_cost = forms.FloatField(help_text = "Third extra service cost")
+    extra3_cost = forms.FloatField(min_value = 0,
+                                   help_text = "Third extra service cost")
     
-    total = forms.FloatField(help_text = "Total cost")
+    total = forms.FloatField(min_value = 0,
+                             help_text = "Total cost")
     
     class Meta:
         model = ProjectBillDetails
