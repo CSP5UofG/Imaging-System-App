8b523bf36bf60127b831cba9f548947a7b534c3f
zsolttakacs2000 <zsolt.takacs2000@gmail.com>
2022-02-02 16:09:59 +0000

    feat: added a very basic example for adding and editing customers using forms

--- imaging_system_app_project/imaging_system_app/urls.py
+++ imaging_system_app_project/imaging_system_app/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('projects/', views.projects, name='projects'),
     path('customers/', views.customers, name='customers'),
     path('bills/', views.bills, name='bills'),
+    path('add-customer/', views.addCustomer, name='add-customer'),
+    path('edit-customer/', views.editCustomer, name='edit-customer'),
 ]
--- imaging_system_app_project/imaging_system_app/views.py
+++ imaging_system_app_project/imaging_system_app/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .helper import FieldLookup
 from .models import Customer, Worker, Services, Bill, ProjectBillDetails, ProjectBillBridge, Project, WorkerProjectBridge
 import datetime
+from imaging_system_app.forms import ServicesForm, CustomerForm, WorkerForm, ProjectForm, WorkerProjectBridgeForm, BillForm, ProjectBillDetailsForm, ProjectBillBridgeForm
+from django.urls import reverse
+
 
 from django.http import HttpResponse
 
@@ -41,6 +44,41 @@ def customers(request):
     context_dict['customers']= customers
 
     return render(request, 'imaging_system_app/customers.html', context=context_dict)
+
+def addCustomer(request):
+    form = CustomerForm
+    context_dict={'form': form}
+    
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+        
+        if form.is_valid():
+            new_customer = form.save()
+            return redirect(reverse('imaging_system_app:customers'))
+    return render(request, 'imaging_system_app/addCustomer.html', context=context_dict)
+
+def editCustomer(request):
+    #find the walk object to edit
+    try:
+        customer = Customer.objects.order_by('cust_id')[:1].first()
+    except Customer.DoesNotExist:
+        customer = None
+    
+    if customer is None:
+        return redirect('/imaging_system_app/')
+    
+    #fill new form with current instance
+    form = CustomerForm(request.POST or None, instance=customer)
+    context_dict={'form': form}
+    
+    if request.method == 'POST':
+        update = CustomerForm(request.POST)
+        
+        if update.is_valid():
+            new_customer = form.save()
+            return redirect(reverse('imaging_system_app:customers'))
+    return render(request, 'imaging_system_app/editCustomer.html', context=context_dict)
+
 
 def bills(request):
     context_dict={}
