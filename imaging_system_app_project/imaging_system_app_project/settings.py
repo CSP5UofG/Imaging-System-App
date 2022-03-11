33c09c056ded3c69ca6ff36f4553f0cec1b0ccab
Forbes Lai <2445570l@student.gla.ac.uk>
2022-03-11 00:13:59 +0000

    feat: add secret_key.txt for deployment

--- imaging_system_app_project/imaging_system_app_project/settings.py
+++ imaging_system_app_project/imaging_system_app_project/settings.py
@@ -23,7 +23,8 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q^&-1&cppmu1j#difyt*80+^&f@9h$!@v-a4%sv3dq+y!6!lbz'
+with open('secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
